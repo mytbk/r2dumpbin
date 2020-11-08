359fdc4ecb1487e874778980490061e630d6b731
Iru Cai <mytbk920423@gmail.com>
2020-11-08 21:34:18 +0800

    Add Broadwell refcode dump script

---
+++ test/ref/dump_refcode_bdw.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys
+import r2pipe
+from dumpbin import R2BinaryDumper
+from dumpbin_util import getReloc
+
+if __name__ == "__main__":
+    r2 = r2pipe.open(sys.argv[1])
+    r2dumpbin = R2BinaryDumper(r2, ["f va @ 0"])
+    r2dumpbin.setReloc(getReloc(sys.argv[2]))
+    r2dumpbin.run_tool()
